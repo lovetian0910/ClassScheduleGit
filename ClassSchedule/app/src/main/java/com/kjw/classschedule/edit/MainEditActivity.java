eaadd5561bf6f9655712f129c401268c45638906
jwkuang <254014188@qq.com>
2016-09-17 22:51:06 +0800

    创建编辑页activity

---
+++ ClassSchedule/app/src/main/java/com/kjw/classschedule/edit/MainEditActivity.java
@@ -0,0 +1,18 @@
+package com.kjw.classschedule.edit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kjw.classschedule.R;
+
+/**
+ * Created by jwkuang on 2016/9/16.
+ */
+public class MainEditActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
